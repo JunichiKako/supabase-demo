e1726ff9b5f29bbb42ba425c915257d64274a407
Junichi Kako <147279646+JunichiKako@users.noreply.github.com>
2024-04-23 01:06:11 +0900

    init

---
+++ datas/posts.ts
@@ -0,0 +1,10 @@
+import "server-only";
+
+import { createClient } from "@/lib/supabase/server";
+
+export const getPosts = async () => {
+    const supabase = createClient();
+    const { data: posts, error } = await supabase.from("posts").select();
+
+    return posts;
+};
